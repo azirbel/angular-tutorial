7bcbbc49b67e07a1604a87da007d8b471bce132b
Alex Zirbel <alexzirbel@gmail.com>
2016-10-04 14:59:25 -0700

    Move COMPS data to analysis route

--- app/components/similar-comps.js
+++ app/components/similar-comps.js
@@ -1,33 +1,10 @@
 import angular from 'angular';
 
-const COMPS = [
-  {
-    street: '116 New Montgomery St',
-    bedrooms: 3,
-    bathrooms: 4,
-    price_cents: 32500000,
-  },
-  {
-    street: '123 Sesame St',
-    bedrooms: 2,
-    bathrooms: 2,
-    price_cents: 18000000,
-  },
-  {
-    street: '3757 Paseo Del Sol',
-    bedrooms: 5,
-    bathrooms: 4,
-    price_cents: 37800000,
-  },
-];
-
 module.exports = angular.module('app.components.similar-comps', [])
 .component('similarComps', {
+  bindings: {
+    compData: '<',
+  },
   template: require('./similar-comps.jade')(),
-  controller: class {
-    constructor() {
-      this.COMPS = COMPS;
-    }
-  }
 })
 .name;
--- app/routes/analysis.js
+++ app/routes/analysis.js
@@ -1,11 +1,42 @@
 import angular from 'angular';
 
+const COMPS = [
+  {
+    street: '116 New Montgomery St',
+    bedrooms: 3,
+    bathrooms: 4,
+    price_cents: 32500000,
+  },
+  {
+    street: '123 Sesame St',
+    bedrooms: 2,
+    bathrooms: 2,
+    price_cents: 18000000,
+  },
+  {
+    street: '3757 Paseo Del Sol',
+    bedrooms: 5,
+    bathrooms: 4,
+    price_cents: 37800000,
+  },
+];
+
 module.exports = angular.module('app.routes.analysis', [])
 .config(function($stateProvider) {
   $stateProvider.state('analysis', {
     url: '/analysis',
     template: require('./analysis.jade')(),
-    controller: class { },
+    resolve: {
+      compData: function() {
+        return COMPS;
+      },
+    },
+    controllerAs: 'analysisCtrl',
+    controller: class {
+      constructor(compData) {
+        this.compData = compData;
+      }
+    },
   });
 })
 .name;
